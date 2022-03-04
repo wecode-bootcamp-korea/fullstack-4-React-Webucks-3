c6ddb3dbee2a5ba6dff5639cbaff9973c525a2de
전하은 <hani@Hanis-Mac-Book.local>
2022-03-04 15:11:54 +0900

    Update: Mission 2 구현

--- src/pages/haeun/Login/Login.js
+++ src/pages/haeun/Login/Login.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 function Login() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const goToList = () => {
-    navigate('/list');
+  // const goToList = () => {
+  //   navigate('/list');
+  // };
+
+  //id 값을 저장할 state 선언
+  const [id, setId] = useState('');
+  //id값을 가져올 함수 선언
+  const getId = e => {
+    setId(e.target.value);
   };
+
+  //password 값을 저장할 state 선언
+  const [password, setPassword] = useState('');
+  //password 값을 가져올 함수 선언
+  const getPW = e => {
+    setPassword(e.target.value);
+  };
+
+  //유효성 검사 함수
+  const isValid = id.includes('@') && password.length >= 5;
+
   return (
-    <div class="loginHaeun">
+    <div className="loginHaeun">
       <div className="card">
         <h1 id="title">Webucks</h1>
         <div className="form">
@@ -17,18 +35,22 @@ function Login() {
             placeholder="전화번호, 사용자 이름 또는 이메일"
             className="cardElements inputElements"
             id="id"
+            onChange={getId}
           />
           <input
             type="password"
             placeholder="비밀번호"
             className="cardElements inputElements"
             id="passWord"
+            onChange={getPW}
           />
         </div>
         <button
-          className="cardElements disabledBtn"
+          className={
+            'cardElements disabledBtn ' + (isValid ? 'activatedBtn' : '')
+          }
           id="loginBtn"
-          onClick={goToList}
+          // onClick={goToList}
         >
           로그인
         </button>
