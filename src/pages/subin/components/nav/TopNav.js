2ba2771aae5375b4cc86b5e53159bf4d67059828
김수빈 <ksb334ksb@gmail.com>
2022-03-05 00:08:18 +0900

    Fix : prettier indent fix

--- src/pages/subin/components/nav/TopNav.js
+++ src/pages/subin/components/nav/TopNav.js
@@ -1,23 +1,31 @@
-import React from "react";
-import "./TopNav.scss"
+import React from 'react';
+import './TopNav.scss';
 
 function TopNav() {
-    return (
-        <div className="TopNavSubin">
-        <header>
+  return (
+    <div className="TopNavSubin">
+      <header>
         <h2 className="title">WeBucks</h2>
         <nav>
-            <ul>
-                {/* a 태그를 div로 임시 변경함 */}
-                <li><div>COFFEE</div></li>
-                <li><div>MENU</div></li>
-                <li><div>STORE</div></li>
-                <li><div>WHAT'S NEW</div></li>
-            </ul>
+          <ul>
+            {/* a 태그를 div로 임시 변경함 */}
+            <li>
+              <div>COFFEE</div>
+            </li>
+            <li>
+              <div>MENU</div>
+            </li>
+            <li>
+              <div>STORE</div>
+            </li>
+            <li>
+              <div>WHAT'S NEW</div>
+            </li>
+          </ul>
         </nav>
-    </header>
+      </header>
     </div>
-    )
+  );
 }
 
 export default TopNav;
